ab0f079a24f694ac3574414f6fb123029281d256
na.altuwaim <noura.tu19@gmail.com>
2025-11-26 09:38:24 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-root">
+    <LiveKitRoom />
+  </div>
+</template>
+
+<script setup>
+import LiveKitRoom from './components/LiveKitRoom.vue'
+</script>
+
+<style scoped>
+.app-root {
+  width: 100%;
+  height: 100%;
+}
+</style>
